4d6e741a9227dd49c441fc992f0b8dead87f41f6
creativcoder <creativcoders@gmail.com>
2020-12-07 09:29:16 +0530

    blog: merge-k-sorted-arrays-rust

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import style from '../styles/newsletter.module.css'
 
 const Footer = ({}) => (
   <footer style={style.newsletter}>
     <div>
       <p>
-        Hey, welcome! I mostly write about Rust, Web, Backend engineering, distributed systems and more. <br />
+        Hey, welcome! I write about Rust, Web, Backend engineering, Distributed systems and more. <br />
         Want to stay updated on my next post? Consider subscribing!</p>
       <iframe src="https://creativcoder.substack.com/embed" width="380" height="320" frameBorder="0" scrolling="no"></iframe>
     </div>
